f8809cf25ea87c35ec1a73ad3cdc7eafa55f8b99
noumi <139501014+noumidev@users.noreply.github.com>
2025-08-21 03:07:46 +0200

    Initialize and step CPU

--- src/nejicast.cpp
+++ src/nejicast.cpp
@@ -7,30 +7,40 @@
 
 #include <cstdio>
 
+#include <hw/cpu/cpu.hpp>
 #include <hw/g1/g1.hpp>
 #include <hw/holly/bus.hpp>
 
 constexpr int NUM_ARGS = 2;
 
+constexpr i64 CYCLES_PER_SLICE = 128;
+
 namespace nejicast {
 
 void initialize(const common::Config& config) {
+    hw::cpu::initialize();
     hw::g1::initialize(config.boot_path);
     hw::holly::bus::initialize();
 }
 
 void shutdown() {
+    hw::cpu::shutdown();
     hw::g1::shutdown();
     hw::holly::bus::shutdown();
 }
 
 void reset() {
+    hw::cpu::reset();
     hw::g1::reset();
     hw::holly::bus::reset();
 }
 
 void run() {
+    while (true) {
+        *hw::cpu::get_cycles() = CYCLES_PER_SLICE;
 
+        hw::cpu::step();
+    }
 }
 
 }
