371be4ede2b9d1b620c59357d55e9b9bb41067c6
noumi <139501014+noumidev@users.noreply.github.com>
2025-08-29 17:55:59 +0200

    PVR: Add a lot of CORE registers

--- src/hw/pvr/core.cpp
+++ src/hw/pvr/core.cpp
@@ -3,6 +3,7 @@
  * Copyright (C) 2025  noumidev
  */
 
+#include "common/types.hpp"
 #include <hw/pvr/core.hpp>
 
 #include <cassert>
@@ -15,11 +16,31 @@
 namespace hw::pvr::core {
 
 enum : u32 {
+    IO_ID              = 0x005F8000,
+    IO_REVISION        = 0x005F8004,
     IO_SOFTRESET       = 0x005F8008,
+    IO_SPAN_SORT_CFG   = 0x005F8030,
     IO_VO_BORDER_COLOR = 0x005F8040,
     IO_FB_R_CTRL       = 0x005F8044,
+    IO_FB_W_CTRL       = 0x005F8048,
+    IO_FB_W_LINESTRIDE = 0x005F804C,
+    IO_FB_R_SIZE       = 0x005F805C,
+    IO_FB_X_CLIP       = 0x005F8068,
+    IO_FB_Y_CLIP       = 0x005F806C,
+    IO_FPU_SHAD_SCALE  = 0x005F8074,
+    IO_FPU_CULL_VAL    = 0x005F8078,
+    IO_FPU_PARAM_CFG   = 0x005F807C,
+    IO_HALF_OFFSET     = 0x005F8080,
+    IO_FPU_PERP_VAL    = 0x005F8084,
+    IO_ISP_BACKGND_D   = 0x005F8088,
+    IO_ISP_BACKGND_T   = 0x005F808C,
+    IO_ISP_FEED_CFG    = 0x005F8098,
     IO_SDRAM_REFRESH   = 0x005F80A0,
     IO_SDRAM_CFG       = 0x005F80A8,
+    IO_FOG_COL_RAM     = 0x005F80B0,
+    IO_FOG_COL_VERT    = 0x005F80B4,
+    IO_FOG_CLAMP_MAX   = 0x005F80BC,
+    IO_FOG_CLAMP_MIN   = 0x005F80C0,
     IO_SPG_HBLANK_INT  = 0x005F80C8,
     IO_SPG_VBLANK_INT  = 0x005F80CC,
     IO_SPG_CONTROL     = 0x005F80D0,
@@ -27,21 +48,56 @@ enum : u32 {
     IO_SPG_LOAD        = 0x005F80D8,
     IO_SPG_VBLANK      = 0x005F80DC,
     IO_SPG_WIDTH       = 0x005F80E0,
+    IO_TEXT_CONTROL    = 0x005F80E4,
     IO_VO_CONTROL      = 0x005F80E8,
     IO_VO_STARTX       = 0x005F80EC,
     IO_VO_STARTY       = 0x005F80F0,
     IO_SPG_STATUS      = 0x005F810C,
+    IO_FB_BURSTCTRL    = 0x005F8110,
 };
 
+#define SPAN_SORT_CFG   ctx.span_sort_configuration
 #define VO_BORDER_COLOR ctx.video_output.border_color
 #define FB_R_CTRL       ctx.frame_buffer.read_control
+#define FB_W_CTRL       ctx.frame_buffer.write_control
+#define FB_W_LINESTRIDE ctx.frame_buffer.line_stride
+#define FB_R_SIZE       ctx.frame_buffer.read_size
+#define FB_X_CLIP       ctx.frame_buffer.x_clip
+#define FB_Y_CLIP       ctx.frame_buffer.y_clip
+#define FPU_SHAD_SCALE  ctx.fpu.shadow_scale
+#define FPU_CULL_VAL    ctx.fpu.cull_value
+#define FPU_PARAM_CFG   ctx.fpu.parameter_configuration
+#define HALF_OFFSET     ctx.half_offset
+#define FPU_PERP_VAL    ctx.fpu.perpendicular_compare_val
+#define ISP_BACKGND_D   ctx.isp.background_depth
+#define ISP_BACKGND_T   ctx.isp.background_tag
+#define ISP_FEED_CFG    ctx.isp.feed_configuration
 #define SDRAM_REFRESH   ctx.sdram.refresh
 #define SDRAM_CFG       ctx.sdram.configuration
+#define FOG_COL_RAM     ctx.fog.color_lut_mode
+#define FOG_COL_VERT    ctx.fog.color_vertex_mode
+#define FOG_CLAMP_MAX   ctx.fog.clamp_max
+#define FOG_CLAMP_MIN   ctx.fog.clamp_min
+#define TEXT_CONTROL    ctx.texture_control
 #define VO_CONTROL      ctx.video_output.control
 #define VO_STARTX       ctx.video_output.horizontal_start
 #define VO_STARTY       ctx.video_output.vertical_start
+#define FB_BURSTCTRL    ctx.frame_buffer.burst_control
 
 struct {
+    union {
+        u32 raw;
+
+        struct {
+            u32 enable_span_sort   :  1;
+            u32                    :  7;
+            u32 enable_offset_sort :  1;
+            u32                    :  7;
+            u32 bypass_cache       :  1;
+            u32                    : 15;
+        };
+    } span_sort_configuration;
+    
     struct {
         union {
             u32 raw;
@@ -59,12 +115,145 @@ struct {
                 u32                     : 8;
             };
         } read_control;
+
+        union {
+            u32 raw;
+
+            struct {
+                u32 format           : 3;
+                u32 enable_dithering : 1;
+                u32                  : 4;
+                u32 k_value          : 8;
+                u32 alpha_threshold  : 8;
+                u32                  : 8;
+            };
+        } write_control;
+
+        union {
+            u32 raw;
+
+            struct {
+                u32 x       : 10;
+                u32 y       : 10;
+                u32 modulus : 10;
+                u32         :  2;
+            };
+        } read_size;
+
+        union {
+            u32 raw;
+
+            struct {
+                u32 clipping_min : 11;
+                u32              :  5;
+                u32 clipping_max : 11;
+                u32              :  5;
+            };
+        } x_clip, y_clip;
+
+        u32 line_stride;
+
+        union {
+            u32 raw;
+
+            struct {
+                u32 read_burst     :  6;
+                u32                :  2;
+                u32 remaining_data :  8;
+                u32 write_burst    :  4;
+                u32                : 12;
+            };
+        } burst_control;
     } frame_buffer;
+
+    union {
+        u32 raw;
+
+        struct {
+            u32 texel_half_offset :  1;
+            u32 pixel_half_offset :  1;
+            u32 fpu_half_offset   :  1;
+            u32                   : 29;
+        };
+    } half_offset;
+
+    struct {
+        union {
+            u32 raw;
+
+            struct {
+                u32 scale_value             :  8;
+                u32 enable_intensity_volume :  1;
+                u32                         : 23;
+            };
+        } shadow_scale;
+
+        f32 cull_value;
+
+        union {
+            u32 raw;
+
+            struct {
+                u32 pointer_first_burst_size      :  4;
+                u32 pointer_burst_size            :  4;
+                u32 isp_parameter_burst_threshold :  6;
+                u32 tsp_parameter_burst_threshold :  6;
+                u32                               :  1;
+                u32 region_header_type            :  1;
+                u32                               : 10;
+            };
+        } parameter_configuration;
+
+        f32 perpendicular_compare_val;
+    } fpu;
+
+    struct {
+        f32 background_depth;
+
+        union {
+            u32 raw;
+
+            struct {
+                u32 tag_offset    :  3;
+                u32 tag_address   : 21;
+                u32 skip          :  3;
+                u32 enable_shadow :  1;
+                u32 bypass_cache  :  1;
+                u32               :  3;
+            };
+        } background_tag;
+
+        union {
+            u32 raw;
+
+            struct {
+                u32 pre_sort_mode            :  1;
+                u32                          :  2;
+                u32 discard_mode             :  1;
+                u32 punch_through_chunk_size : 10;
+                u32 translucency_cache_size  : 10;
+                u32                          :  8;
+            };
+        } feed_configuration;
+    } isp;
 
     struct {
         u32 refresh;
         u32 configuration;
     } sdram;
+
+    struct {
+        union {
+            u32 raw;
+
+            struct {
+                u32 blue  : 8;
+                u32 green : 8;
+                u32 red   : 8;
+                u32 alpha : 8;
+            };
+        } color_lut_mode, color_vertex_mode, clamp_max, clamp_min;
+    } fog;
 
     struct {
         union {
@@ -108,6 +297,20 @@ struct {
             };
         } vertical_start;
     } video_output;
+
+    union {
+        u32 raw;
+
+        struct {
+            u32 stride               : 5;
+            u32                      : 3;
+            u32 bank_bit             : 5;
+            u32                      : 3;
+            u32 index_endianness     : 1;
+            u32 code_book_endianness : 1;
+            u32                      : 14;
+        };
+    } texture_control;
 } ctx;
 
 void initialize() {
@@ -128,9 +331,20 @@ T read(const u32 addr) {
     exit(1);
 }
 
+constexpr u32 CORE_ID = 0x17FD11DB;
+constexpr u32 CORE_REVISION = 0x11;
+
 template<>
 u32 read(const u32 addr) {
     switch (addr) {
+        case IO_ID:
+            std::puts("ID read32");
+
+            return CORE_ID;
+        case IO_REVISION:
+            std::puts("REVISION read32");
+
+            return CORE_REVISION;
         case IO_VO_BORDER_COLOR:
             std::puts("VO_BORDER_COLOR read32");
 
@@ -165,10 +379,80 @@ void write(const u32 addr, const u32 data) {
         case IO_SOFTRESET:
             std::printf("SOFTRESET write32 = %08X\n", data);
             break;
+        case IO_SPAN_SORT_CFG:
+            std::printf("SPAN_SORT_CFG write32 = %08X\n", data);
+        
+            SPAN_SORT_CFG.raw = data;
+            break;
         case IO_FB_R_CTRL:
             std::printf("FB_R_CTRL write32 = %08X\n", data);
         
             FB_R_CTRL.raw = data;
+            break;
+        case IO_FB_W_CTRL:
+            std::printf("FB_W_CTRL write32 = %08X\n", data);
+        
+            FB_W_CTRL.raw = data;
+            break;
+        case IO_FB_W_LINESTRIDE:
+            std::printf("FB_W_LINESTRIDE write32 = %08X\n", data);
+        
+            FB_W_LINESTRIDE = data;
+            break;
+        case IO_FB_R_SIZE:
+            std::printf("FB_R_SIZE write32 = %08X\n", data);
+        
+            FB_R_SIZE.raw = data;
+            break;
+        case IO_FB_X_CLIP:
+            std::printf("FB_X_CLIP write32 = %08X\n", data);
+        
+            FB_X_CLIP.raw = data;
+            break;
+        case IO_FB_Y_CLIP:
+            std::printf("FB_Y_CLIP write32 = %08X\n", data);
+        
+            FB_Y_CLIP.raw = data;
+            break;
+        case IO_FPU_SHAD_SCALE:
+            std::printf("FPU_SHAD_SCALE write32 = %08X\n", data);
+        
+            FPU_SHAD_SCALE.raw = data;
+            break;
+        case IO_FPU_CULL_VAL:
+            std::printf("FPU_CULL_VAL write32 = %08X\n", data);
+        
+            FPU_CULL_VAL = to_f32(data);
+            break;
+        case IO_FPU_PARAM_CFG:
+            std::printf("FPU_PARAM_CFG write32 = %08X\n", data);
+        
+            FPU_PARAM_CFG.raw = data;
+            break;
+        case IO_HALF_OFFSET:
+            std::printf("HALF_OFFSET write32 = %08X\n", data);
+        
+            HALF_OFFSET.raw = data;
+            break;
+        case IO_FPU_PERP_VAL:
+            std::printf("FPU_PERP_VAL write32 = %08X\n", data);
+        
+            FPU_PERP_VAL = to_f32(data);
+            break;
+        case IO_ISP_BACKGND_D:
+            std::printf("ISP_BACKGND_D write32 = %08X\n", data);
+        
+            ISP_BACKGND_D = to_f32(data);
+            break;
+        case IO_ISP_BACKGND_T:
+            std::printf("ISP_BACKGND_T write32 = %08X\n", data);
+        
+            ISP_BACKGND_T.raw = data;
+            break;
+        case IO_ISP_FEED_CFG:
+            std::printf("ISP_FEED_CFG write32 = %08X\n", data);
+        
+            ISP_FEED_CFG.raw = data;
             break;
         case IO_SDRAM_REFRESH:
             std::printf("SDRAM_REFRESH write32 = %08X\n", data);
@@ -179,6 +463,26 @@ void write(const u32 addr, const u32 data) {
             std::printf("SDRAM_CFG write32 = %08X\n", data);
         
             SDRAM_CFG = data;
+            break;
+        case IO_FOG_COL_RAM:
+            std::printf("FOG_COL_RAM write32 = %08X\n", data);
+        
+            FOG_COL_RAM.raw = data;
+            break;
+        case IO_FOG_COL_VERT:
+            std::printf("FOG_COL_VERT write32 = %08X\n", data);
+        
+            FOG_COL_VERT.raw = data;
+            break;
+        case IO_FOG_CLAMP_MAX:
+            std::printf("FOG_CLAMP_MAX write32 = %08X\n", data);
+        
+            FOG_CLAMP_MAX.raw = data;
+            break;
+        case IO_FOG_CLAMP_MIN:
+            std::printf("FOG_CLAMP_MIN write32 = %08X\n", data);
+        
+            FOG_CLAMP_MIN.raw = data;
             break;
         case IO_SPG_HBLANK_INT:
             std::printf("SPG_HBLANK_INT write32 = %08X\n", data);
@@ -215,6 +519,11 @@ void write(const u32 addr, const u32 data) {
         
             spg::set_width(data);
             break;
+        case IO_TEXT_CONTROL:
+            std::printf("TEXT_CONTROL write32 = %08X\n", data);
+        
+            TEXT_CONTROL.raw = data;
+            break;
         case IO_VO_CONTROL:
             std::printf("VO_CONTROL write32 = %08X\n", data);
         
@@ -229,6 +538,11 @@ void write(const u32 addr, const u32 data) {
             std::printf("VO_STARTY write32 = %08X\n", data);
         
             VO_STARTY.raw = data;
+            break;
+        case IO_FB_BURSTCTRL:
+            std::printf("FB_BURSTCTRL write32 = %08X\n", data);
+        
+            FB_BURSTCTRL.raw = data;
             break;
         default:
             std::printf("Unmapped PVR CORE write32 @ %08X = %08X\n", addr, data);
