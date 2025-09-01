d7221e081cafae0f28bc75acd8690f8579247972
noumi <139501014+noumidev@users.noreply.github.com>
2025-09-01 15:01:31 +0200

    Update ta.hpp

--- include/hw/pvr/ta.hpp
+++ include/hw/pvr/ta.hpp
@@ -18,9 +18,12 @@ void set_allocation_control(const u32 data);
 void set_global_tile_clip(const u32 data);
 void set_isp_list_base(const u32 data);
 void set_isp_list_limit(const u32 data);
+void set_next_object_pointer_block(const u32 data);
 void set_object_list_base(const u32 data);
 void set_object_list_limit(const u32 data);
 
 void initialize_lists();
+
+void fifo_block_write(const u8* bytes);
 
 }
