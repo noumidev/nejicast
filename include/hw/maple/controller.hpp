ecbee613463ae64924f4f7e21d30e6d900c05a46
noumi <139501014+noumidev@users.noreply.github.com>
2026-01-23 05:42:34 +0100

    MAPLE: make get_device_info a bit prettier

--- include/hw/maple/controller.hpp
+++ include/hw/maple/controller.hpp
@@ -24,18 +24,20 @@ public:
     void get_device_info(Frame& frame) override {
         assert((frame.recipient_addr & 63) == 0x20);
 
-        frame.receive_bytes.push_back(MAPLE_DEVICE_CONTROLLER);
-        frame.receive_bytes.push_back(0xFE060F00);
-        frame.receive_bytes.push_back(0x00000000);
-        frame.receive_bytes.push_back(0x724400FF);
+        frame.receive_bytes.resize(48);
 
-        // TODO
-        for (int i = 0; i < 44; i++) {
-            frame.receive_bytes.push_back(0);
-        }
+        frame.receive_bytes[0] = MAPLE_DEVICE_CONTROLLER;
+        frame.receive_bytes[1] = 0xFE060F00;
+        frame.receive_bytes[2] = 0x00000000;
+        frame.receive_bytes[3] = 0x724400FF;
+
+        // TODO: fill out remaining structures
+
+        constexpr const char* STRING = "Dreamcast Controller         ";
+
+        std::memcpy(((u8*)frame.receive_bytes.data()) + 18, STRING, 29);
 
         frame.sender_addr = 0x20;
-
         frame.result_code = 0x05;
     }
 
@@ -49,7 +51,6 @@ public:
         frame.receive_bytes.push_back(0x80808080);
 
         frame.sender_addr = 0x20;
-
         frame.result_code = 0x08;
     }
 };
